125823a0e3fa5fd1891b43347cda833deb092a23
Martin Luo <martinluo22@hotmail.com>
2017-05-07 17:49:58 -0400

    Add textbox for grocery item input and buttons needed

--- app/src/main/java/io/martinluo/groceryhelper/HomeScreenActivity.java
+++ app/src/main/java/io/martinluo/groceryhelper/HomeScreenActivity.java
@@ -3,6 +3,13 @@ package io.martinluo.groceryhelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class HomeScreenActivity extends AppCompatActivity {
 
@@ -14,4 +21,21 @@ public class HomeScreenActivity extends AppCompatActivity {
         Toolbar mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(mainToolbar);
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar, menu);
+        return true;
+    }
+
+    public void onGroupItemClick(MenuItem item) {
+        // One of the group items (using the onClick attribute) was clicked
+        // The item parameter passed here indicates which item it is
+        // All other menu item clicks are handled by onOptionsItemSelected()
+    }
+
+
+
+
+
 }
