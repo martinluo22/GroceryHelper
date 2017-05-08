ae85a0449293f9f28d6d84bf4c264597bf23538f
Martin Luo <martinluo22@hotmail.com>
2017-05-07 20:16:01 -0400

    Add click listeners for buttons

--- app/src/main/java/io/martinluo/groceryhelper/HomeScreenActivity.java
+++ app/src/main/java/io/martinluo/groceryhelper/HomeScreenActivity.java
@@ -7,11 +7,17 @@ import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class HomeScreenActivity extends AppCompatActivity {
+
+    private static boolean isItemRecurring = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,10 @@ public class HomeScreenActivity extends AppCompatActivity {
 
         Toolbar mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(mainToolbar);
+
+        setupButtonListeners();
+
+
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,7 +44,43 @@ public class HomeScreenActivity extends AppCompatActivity {
         // All other menu item clicks are handled by onOptionsItemSelected()
     }
 
+    public void setupButtonListeners(){
 
+        final EditText editText = (EditText) findViewById(R.id.newItem);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+
+                    addNewItem(isItemRecurring);
+
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+
+        final ImageButton enterButton = (ImageButton) findViewById(R.id.enter_button);
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                addNewItem(isItemRecurring);
+            }
+        });
+
+        final Button recurringButton = (Button) findViewById(R.id.recurring_button);
+        recurringButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                isItemRecurring = !isItemRecurring;
+                return;
+            }
+        });
+
+    }
+
+    public void addNewItem(Boolean isRecurring){
+
+    }
 
 
 
